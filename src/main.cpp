4e70558b6b527ce0c544283e1a7ab0a75e0b317b
mpw-eng <53257685+mpw-eng@users.noreply.github.com>
2025-03-04 21:55:35 +0800

    more accurate frequency calculation

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@
 #define SERVICE_UUID "12345678-1234-5678-1234-56789abcdef0"
 #define CHARACTERISTIC_UUID "12345678-1234-5678-1234-56789abcdef1"
 #define HALL_SENSOR_PIN 4
-#define UPDATE_INTERVAL 1000
-#define DEBOUNCE_US 1000
+#define UPDATE_INTERVAL 1000000  // update interval (us)
+#define DEBOUNCE_US 1000  // debounce time (us)
 
 
 
@@ -22,13 +22,16 @@ bool deviceConnected = false;
 // Global variables
 volatile unsigned long pulseCount = 0;
 volatile unsigned long lastPulseTime = 0;
+volatile unsigned long accumPulseTime = 0;
 unsigned long lastUpdateTime = 0;
 
 
 void countPulse() {
     unsigned long currentTime = micros();
-    if ((currentTime - lastPulseTime) > DEBOUNCE_US) {
+    unsigned long elapsedTime = currentTime - lastPulseTime;
+    if ((elapsedTime > DEBOUNCE_US) && (elapsedTime < 100000000)) {
         pulseCount++;
+        accumPulseTime += elapsedTime;
     }
     lastPulseTime = currentTime;
 }
@@ -36,17 +39,6 @@ void countPulse() {
 void initializeHallSensor() {
     pinMode(HALL_SENSOR_PIN, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(HALL_SENSOR_PIN), countPulse, FALLING);
-  }
-
-float getHz() {
-    
-    noInterrupts();
-    unsigned long currentPulseCount = pulseCount;
-    pulseCount = 0;
-    interrupts();
-
-    unsigned long elapsedTime = millis() - lastUpdateTime;
-    return currentPulseCount * (1000.0 / elapsedTime);
   }
 
 
@@ -62,10 +54,8 @@ class MyServerCallbacks : public BLEServerCallbacks {
 };
 
 
-
 void setup() {
     Serial.begin(115200);
-    
 
     // Initialize BLE
     // BLEDevice::init("Jetblack Power Meter");
@@ -89,19 +79,32 @@ void setup() {
 }
 
 
-
 void loop() {
     // Read speed data from the hall-effect sensor
-    unsigned long currentTime = millis();
+    unsigned long currentTime = micros();
 
-    
-  
     if (currentTime - lastUpdateTime >= UPDATE_INTERVAL) {
-    float Hz = getHz();
-      lastUpdateTime = currentTime;
+        float Hz = 0;
 
-      Serial.print("Freq: ");
-      Serial.println(Hz);
+        // Read interrupt pulse count & reset
+        noInterrupts();
+        unsigned long currentPulseCount = pulseCount;
+        unsigned long currentAccumPulseTime = accumPulseTime;
+        pulseCount = 0;
+        accumPulseTime = 0;
+        interrupts();
+        
+        // Calculate frequency
+        //unsigned long elapsedTime = currentTime - lastUpdateTime;
+        //float Hz = currentPulseCount * (1000000.0 / elapsedTime);
+        if (currentPulseCount > 0){
+            Hz = currentPulseCount * (1000000.0 / currentAccumPulseTime);
+        } 
+        
+        lastUpdateTime = currentTime;
+        
+        Serial.print("Freq: ");
+        Serial.println(Hz);
 
           // Broadcast power meter data if a device is connected
         // if (deviceConnected) {
